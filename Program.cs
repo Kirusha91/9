cad9b233cc84b3c942b488246c99be7378543bf5
Kirusha91 <lotrr@yandex.ru>
2022-12-17 15:49:30 +0300

    2 задачи

--- Program.cs
+++ Program.cs
@@ -1,78 +1,67 @@
-﻿// Задача 19
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
 
-// Напишите программу, которая принимает на
-//  вход пятизначное число и проверяет,
-//   является ли оно палиндромом.
+// 3, 5 -> 243 (3⁵)
 
-// 14212 -> нет
+// 2, 4 -> 16
 
-// 12821 -> да
+//   Console.Write("Введите число A: ");
+//   int numA = Convert.ToInt32(Console.ReadLine());
+//   Console.Write("Введите число B: ");
+//   int numB = Convert.ToInt32(Console.ReadLine());
 
-// // 23432 -> да
-
-Console.Write("Введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-int temp = n;
-int revers = 0;
-
-int met(int num){
-    while (n > 0){
-        int d = n % 10;
-        revers = revers * 10 + d; // Переворачиваем число задом на перед
-        n = n / 10;
-    }
-    return revers;
+//   int exp = met(numA, numB);
+//   Console.WriteLine($"Ответ:   {exp}");
 
 
 
+// int met(int numA, int numB){
 
-//         // if(temp == revers){// проверяем новое число со старым
-//         //     Console.WriteLine($"Ваше число: {n} это палиндром.");
-//         // }
-//         // else Console.WriteLine($"Ваше число: {n} это не палиндром.");
-    
+//   int res = 1;
 
+//   for(int i=1; i <= numB; i++){
 
-}
-if(temp == revers){// проверяем новое число со старым
-            Console.WriteLine($"Ваше число: {n} это палиндром.");
-        }
-        else Console.WriteLine($"Ваше число: {n} это не палиндром.");
-int result = met(num);
+//     res = res * numA;
 
+//   }
 
-
-// Задача 21
-
-// Напишите программу, которая принимает на вход 
-// координаты двух точек и находит расстояние между 
-// ними в 3D пространстве.
-
-// A (3,6,8); B (2,1,-7), -> 15.84
-
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-// double fd( double xA, double yA, double zA, double xB, double yB, double zB){
-
-    
-//     double dis = Math.Sqrt(Math.Pow(xA - xB ,2) + Math.Pow(xB - yB ,2) + Math.Pow(zA - zB ,2));
-
-//     return dis;
+//     return res;
 // }
 
-// Console.WriteLine("Введите Координаты точки zA: ");
-// double xA = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите Координаты точки yA: ");
-// double yA = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите Координаты точки zA: ");
-// double zA = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите Координаты точки xB: ");
-// double xB = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите Координаты точки yB: ");
-// double yB = Convert.ToDouble(Console.ReadLine());
-// Console.WriteLine("Введите Координаты точки zB: ");
-// double zB = Convert.ToDouble(Console.ReadLine());
- 
 
-//  double dist = fd(xA, yA, zA, xB, yB, zB);
-//  Console.WriteLine($"Ваша дистанция = {dist}");
+
+// Задача 27: Напишите программу, которая принимает на вход число и
+//  выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int Sum(int num){
+    
+    // int count = 0;
+    int a = 0;
+    int res = 0;
+
+    for (int count = 0; count <= num; count++){
+      a = num - num % 10;
+      res = res + (num - a);
+      num = num / 10;
+    }
+   return res;
+  }
+
+int sum = Sum(num);
+Console.WriteLine("Сумма цифр в числе: " + sum);
+
+
+
+
+
+
+
