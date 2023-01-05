d6cd097dc02a75d4d2433934250596ee7e676ef4
Kirusha91 <lotrr@yandex.ru>
2023-01-05 12:54:17 +0300

    zada4a 1 izm

--- Program.cs
+++ Program.cs
@@ -1,14 +1,4 @@
-﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный 
-// случайными вещественными числами.
-
-// m = 3, n = 4.
-
-// 0,5 7 -2 -0,2
-
-// 1 -3,3 8 -9,9
-
-// 8 7,8 -7,1 9
-
+﻿
 Console.Write("Введите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
@@ -42,10 +32,8 @@ for (int i = 0; i < m; i++)
       for (int j = 0; j < n; j++)
       {
         double alignNumber = Math.Round(array[i, j], 1);
-        Console.Write(alignNumber + " ");
+        Console.Write(alignNumber + "\t");
       }
       Console.WriteLine();
   }
 }
-
-
